33b85842cba4b96345ae9ac04d3c938fa3e78d05
itzJunaedRayhan <webgenesis99@gmail.com>
2024-09-23 10:58:04 +0600

    [DONE] user created successfully

--- src/app/modules/users/users.services.ts
+++ src/app/modules/users/users.services.ts
@@ -1,23 +1,23 @@
-import config from '../../../config'
-import { IUser } from './users.interface'
-import { User } from './users.model'
-import { generateUserId } from './users.utils'
+import config from '../../../config';
+import { IUser } from './users.interface';
+import { User } from './users.model';
+import { generateUserId } from './users.utils';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
-  const id = await generateUserId()
+  const id = await generateUserId();
   // eslint-disable-next-line no-console
-  console.log(user)
-  user.id = id
+  console.log(user);
+  user.id = id;
   if (!user.password) {
-    user.password = config.default_user_pass as string
+    user.password = config.default_user_pass as string;
   }
-  const createdUser = await User.create(user)
+  const createdUser = await User.create(user);
   if (!createdUser) {
-    throw new Error(`Failed to create user!...`)
+    throw new Error(`Failed to create user!...`);
   }
-  return createdUser
-}
+  return createdUser;
+};
 
 export default {
   createUser,
-}
+};
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 /* eslint-disable no-console */
-import mongoose from 'mongoose'
-import config from './config/index'
-import app from './app'
+import mongoose from 'mongoose';
+import config from './config/index';
+import app from './app';
 
 const bootstrap = async (): Promise<void> => {
   try {
-    await mongoose.connect(config.database_url as string)
-    console.log('Database connected Successfully...')
+    await mongoose.connect(config.database_url as string);
+    console.log('Database connected Successfully...');
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
+      console.log(`Example app listening on port ${config.port}`);
+    });
   } catch (err) {
-    console.log('There was an error connecting to database', err)
+    console.log('There was an error connecting to database', err);
   }
-}
+};
 
-bootstrap()
+bootstrap();
