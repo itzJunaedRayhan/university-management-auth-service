bc9885005130395d00f9229730724f606c122f6c
itzJunaedRayhan <webgenesis99@gmail.com>
2024-10-11 20:29:38 +0600

    [DONE] University-Management-Auth

--- src/server.ts
+++ src/server.ts
@@ -2,33 +2,32 @@
 import mongoose from 'mongoose';
 import config from './config/index';
 import app from './app';
-import { errorLogger, logger } from './shared/logger';
 import { Server } from 'http';
 
 //  Handle  - Uncaught exceptions:
 let server: Server;
 process.on('uncaughtException', error => {
-  errorLogger.error(error);
+  console.log(error);
   process.exit(1);
 });
 
 const bootstrap = async (): Promise<void> => {
   try {
     await mongoose.connect(config.database_url as string);
-    logger.info('Database connected Successfully...');
+    console.log('Database connected Successfully...');
 
     app.listen(config.port, () => {
-      logger.info(`Example app listening on port ${config.port}`);
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
-    errorLogger.error('There was an error connecting to database', err);
+    console.log('There was an error connecting to database', err);
   }
 
   //  Handle - unhandle Rejection:
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorLogger.error(error);
+        console.log(error);
         process.exit(1);
       });
     } else {
@@ -41,7 +40,7 @@ bootstrap();
 
 //  Close Server if SIGNAL is Terminated
 process.on(`SIGTERM`, () => {
-  logger.info('SIGTERM is received');
+  console.log('SIGTERM is received');
   if (server) {
     server.close();
   }
